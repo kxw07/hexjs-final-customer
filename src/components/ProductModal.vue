9d7fb94649215126ec3143bd8dff6ee2594b937c
kai <kai@simbo.com.tw>
2020-08-16 15:13:03 +0800

    Modify productModal

---
+++ src/components/ProductModal.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="modal fade" id="productModal" tabindex="-1" role="dialog" aria-labelledby="productModalLabel"
+       aria-hidden="true" v-show="value">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="productModalLabel">{{ product.title }}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div style="height: 200px; background-size: cover; background-position: center"
+               :style="style"></div>
+          <blockquote class="blockquote mt-3">
+            <p class="mb-0" v-html="product.content"></p>
+            <footer class="blockquote-footer text-right">
+              {{ product.description }}
+            </footer>
+          </blockquote>
+          <div class="d-flex justify-content-between align-items-baseline">
+            <div v-if="!product.price" class="h4">
+              {{ product.origin_price }} 元
+            </div>
+            <del v-if="product.price" class="h6">原價 {{ product.origin_price }} 元</del>
+            <div v-if="product.price" class="h4">
+              現在只要 {{ product.price }} 元
+            </div>
+          </div>
+
+          <select v-model="product.num" name class="form-control mt-3">
+            <option value="0" disabled selected>
+              請選擇數量
+            </option>
+            <option v-for="num in 5" :key="num" :value="num">
+              選購 {{ num }} {{ product.unit }}
+            </option>
+          </select>
+        </div>
+        <div class="modal-footer">
+          <div v-if="product.num" class="text-muted text-nowrap mr-3">
+            小計
+            <strong>{{ product.num * product.price }}</strong> 元
+          </div>
+          <button type="button" class="btn btn-primary" v-on:click="addToCart(product)">
+            <!--            <i v-if="product.id === status.loadingItem" class="fas fa-spinner fa-spin"></i>-->
+            加到購物車
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductModal',
+  data () {
+    return {}
+  },
+  props: {
+    value: {
+      required: true
+    },
+    product: {}
+  },
+  created () {
+  },
+  mounted () {
+  },
+  computed: {
+    style () {
+      return `backgroundImage: url(${this.product.imageUrl})`
+    }
+  },
+  methods: {
+    addToCart (product) {
+      this.$bus.$emit('addToCart', JSON.parse(JSON.stringify(product)))
+      this.$('#productModal').modal('hide')
+    },
+    closeModal () {
+      this.$emit('closeModal')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,34 +1,35 @@
 <template>
   <div id="app" class="container">
     <div class="row mt-4">
-      <div v-for="item in products" :key="item.id" class="col-md-4 mb-4">
+      <div v-for="product in products" :key="product.id" class="col-md-4 mb-4">
         <div class="card border-0 shadow-sm">
           <div style="height: 150px; background-size: cover; background-position: center"
-               :style="{backgroundImage: `url(${item.imageUrl})`}"></div>
+               :style="{backgroundImage: `url(${product.imageUrl})`}"></div>
           <div class="card-body">
-            <span class="badge badge-secondary float-right ml-2">{{ item.category }}</span>
-            <h5 class="card-title">{{ item.title }}</h5>
-            <p class="card-text">{{ item.content }}</p>
+            <span class="badge badge-secondary float-right ml-2">{{ product.category }}</span>
+            <h5 class="card-title">{{ product.title }}</h5>
+            <p class="card-text">{{ product.content }}</p>
             <div class="d-flex justify-content-between align-items-baseline">
-              <div v-if="!item.price" class="h5">
-                {{ item.origin_price }} 元
+              <div v-if="!product.price" class="h5">
+                {{ product.origin_price }} 元
               </div>
               <div v-else>
-                <del class="h6">原價 {{ item.origin_price }} 元</del>
+                <del class="h6">原價 {{ product.origin_price }} 元</del>
                 <div class="h5">
-                  現在只要 {{ item.price }} 元
+                  現在只要 {{ product.price }} 元
                 </div>
               </div>
             </div>
           </div>
-          <!--          <div class="card-footer d-flex">-->
-          <!--            <button type="button" class="btn btn-outline-secondary btn-sm" v-on:click="getProductDetail(item.id)">-->
-          <!--              查看更多-->
-          <!--            </button>-->
-          <!--            <button type="button" class="btn btn-outline-danger btn-sm ml-auto" v-on:click="addToCart(item)">-->
-          <!--              加到購物車-->
-          <!--            </button>-->
-          <!--          </div>-->
+          <div class="card-footer d-flex">
+            <product-modal v-model="modalOpen" :product="product" v-on:close-modal="reverseModalState"></product-modal>
+            <button type="button" class="btn btn-outline-secondary btn-sm" v-on:click="getProductDetail(product.id)">
+              查看更多
+            </button>
+            <!--            <button type="button" class="btn btn-outline-danger btn-sm ml-auto" v-on:click="addToCart(product)">-->
+            <!--              加到購物車-->
+            <!--            </button>-->
+          </div>
         </div>
       </div>
     </div>
@@ -36,24 +37,29 @@
 </template>
 
 <script>
+import productModal from '@/components/ProductModal'
+
 export default {
   name: 'Products',
+  components: {
+    productModal
+  },
   data () {
     return {
-      uuid: 'de4aaa47-9e6a-44fb-be33-178ff34efbdc',
       isLoading: true,
+      modalOpen: false,
       products: [],
       product: {}
     }
   },
   created () {
-    this.axios.get(`https://course-ec-api.hexschool.io/api/${this.uuid}/ec/products`)
+    this.axios.get(`https://course-ec-api.hexschool.io/api/${process.env.VUE_APP_UUID}/ec/products`)
       .then(res => {
         this.isLoading = false
         this.products = res.data.data
       })
       .catch(err => {
-        console.err(err)
+        console.error(err)
       })
   },
   mounted () {
@@ -61,17 +67,20 @@ export default {
   },
   methods: {
     getProductDetail (productId) {
-      this.isLoading = true
-      this.axios.get(`https://course-ec-api.hexschool.io/api/${this.uuid}/ec/product/${productId}`)
-        .then(res => {
-          this.isLoading = false
-          this.product = res.data.data
-          this.product.num = 0
-          this.$('#productModal').modal('show')
-        })
-        .catch(err => {
-          console.err(err)
-        })
+      this.reverseModalState()
+      // this.isLoading = true
+      // this.axios.get(`https://course-ec-api.hexschool.io/api/${process.env.VUE_APP_UUID}/ec/product/${productId}`)
+      //   .then(res => {
+      //     this.isLoading = false
+      //     this.product = res.data.data
+      //     this.product.num = 0
+      //     // this.$('#productModal').modal('show')
+      //     this.reverseModalState()
+      //     console.log(this.modalOpen)
+      //   })
+      //   .catch(err => {
+      //     console.error(err)
+      //   })
     },
     addToCart (product, quantity = 1) {
       product.num = quantity
@@ -79,6 +88,10 @@ export default {
     },
     deleteItem (itemIndex) {
       this.shoppingList.splice(itemIndex, 1)
+    },
+    reverseModalState () {
+      console.log(this.modalOpen)
+      this.modalOpen = !this.modalOpen
     }
   }
 }
