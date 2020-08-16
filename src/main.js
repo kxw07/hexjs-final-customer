8b9465b5ec6f2980c961a5ec3e78ec985946f1da
kai <kai@simbo.com.tw>
2020-08-16 13:48:53 +0800

    Add modules

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+import _ from 'lodash'
+import $ from 'jquery'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
+Vue.use(Loading)
+
+Vue.prototype._ = _
+Vue.prototype.$ = $
 
 new Vue({
   router,
