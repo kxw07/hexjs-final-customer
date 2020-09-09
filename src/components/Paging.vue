628b15d37b6fa76b74e70e7a39365341116db952
kai <kai@simbo.com.tw>
2020-09-09 22:23:19 +0800

    add page order

---
+++ src/components/Paging.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="btn-group" role="group" aria-label="Basic example">
+    <button type="button" class="btn btn-primary" :disabled="hasPrevPage()" :class="{'disabled': hasPrevPage()}" v-on:click="changePage(pagination.current_page - 1)">上一頁</button>
+    <button type="button" class="btn btn-light" :disabled="true">{{pagination.current_page}}</button>
+    <button type="button" class="btn btn-primary" :disabled="hasNextPage()" :class="{'disabled': hasNextPage()}" v-on:click="changePage(pagination.current_page + 1)">下一頁</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'paging',
+  data () {
+    return {
+    }
+  },
+  props: {
+    pagination: {}
+  },
+  methods: {
+    hasPrevPage () {
+      return this.pagination.current_page === 1
+    },
+    hasNextPage () {
+      return this.pagination.current_page > this.pagination.total_pages || this.pagination.current_page === this.pagination.total_pages
+    },
+    changePage (expectPage) {
+      this.$emit('change-page', expectPage)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,15 +1,108 @@
 <template>
-  <div>
-    <h1>This is orders page.</h1>
+  <div id="app" class="container custom-container-width">
+    <h2>訂單列表</h2>
+    <div>
+      <table class="table">
+        <thead>
+        <tr>
+          <th width="60">項次</th>
+          <th width="300">訂單編號</th>
+          <th width="200">購買商品</th>
+          <th width="120">優惠券</th>
+          <th width="100">總價</th>
+          <th width="120">付款方式</th>
+          <th width="120">建立時間</th>
+          <th width="120">更新時間</th>
+          <th width="120">付款狀態</th>
+          <th>編輯</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(order, index) in orders" :key="order.id">
+          <td>{{ index + 1 }}</td>
+          <td style="word-break:break-all; word-wrap:break-all;">{{ order.id }}</td>
+          <td>
+            <ul v-for="(product, idx) in order.products" :key="idx">
+              <li>{{ product.product.title }} x {{ product.quantity }}</li>
+            </ul>
+          </td>
+          <td>{{ order.coupon ? order.coupon.code : '' }}</td>
+          <td class="text-right">{{ order.amount }}</td>
+          <td>{{ order.payment }}</td>
+          <td>{{ order.created.datetime }}</td>
+          <td>{{ order.updated.datetime }}</td>
+          <td>
+            <span v-if="order.paid" class="text-success">已付款</span>
+            <span v-else class="text-danger">尚未付款</span>
+          </td>
+          <td>
+            <button v-if="order.paid == false" type="button" v-on:click="pay(order)"
+                    class="btn btn-outline-primary btn-sm">付款
+            </button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <paging class="paging" :pagination="pagination" v-on:change-page="getOrders"></paging>
   </div>
 </template>
 
 <script>
+import paging from '@/components/Paging.vue'
+
 export default {
-  name: 'Orders'
+  name: 'Orders',
+  components: {
+    paging
+  },
+  data () {
+    return {
+      orders: [],
+      pagination: {}
+    }
+  },
+  created () {
+    this.getOrders()
+  },
+  methods: {
+    getOrders (page = this.pagination.current_page || 1) {
+      const loader = this.$loading.show()
+
+      this.axios.get(`${process.env.VUE_APP_API_URL}/api/${process.env.VUE_APP_UUID}/ec/orders?page=${page}`
+      ).then(res => {
+        this.orders = this._.sortBy(res.data.data, ['updated', 'created'])
+        this.pagination = res.data.meta.pagination
+        loader.hide()
+      }).catch(err => {
+        console.error(err)
+        loader.hide()
+      })
+    },
+    pay (order) {
+      const loader = this.$loading.show()
+
+      this.axios.post(`${process.env.VUE_APP_API_URL}/api/${process.env.VUE_APP_UUID}/ec/orders/${order.id}/paying`
+      ).then(res => {
+        this.getOrders(this.pagination.current_page)
+        loader.hide()
+      }).catch(err => {
+        console.error(err)
+        loader.hide()
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+.custom-container-width {
+  max-width: 75vw;
+}
 
+ul {
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+}
 </style>
